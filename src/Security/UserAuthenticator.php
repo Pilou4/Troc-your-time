ded40340207a4cac04e47c8cdb201358ae24ebe0
Pilou4 <frederic.caffier@hotmail.com>
2022-03-25 14:59:05 +0100

    page login

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -50,7 +50,7 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        return new RedirectResponse($this->urlGenerator->generate('homepage'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
